6c9489df3140fb1e8903d374230aea533ef6b8bf
Mujahid Anuar <17759705+mujahidfa@users.noreply.github.com>
2023-01-08 17:39:54 +0800

    Init

---
+++ packages/demo/src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+import "./assets/main.css";
+
+import { ThroughPoints, App as Appp, Paa } from "vue-lib";
+
+createApp(App).mount("#app");
---
+++ packages/vue-lib/src/index.ts
@@ -0,0 +1,12 @@
+// import App from "./App.vue";
+
+// export { ThroughPoints } from "./Test";
+
+// export { App };
+export { ThroughPoints } from "./Test";
+// export { default as App } from "./App.vue";
+// export { default as Paa } from "./Paa.vue";
+
+import App from "./App.vue";
+import Paa from "./Paa.vue";
+export { App, Paa };
